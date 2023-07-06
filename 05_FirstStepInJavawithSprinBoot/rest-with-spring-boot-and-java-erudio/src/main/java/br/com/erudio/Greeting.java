3592b6aa65860f94991778eea345fb0aa3afd9a7
Vitor <vytroxalberto@gmail.com>
2023-07-05 23:40:06 -0300

    First Steps in Java with Spring Boot

---
+++ 05_FirstStepInJavawithSprinBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/Greeting.java
@@ -0,0 +1,22 @@
+package br.com.erudio;
+
+public class Greeting {
+	
+	private final long id;
+	private final String content;
+	
+	public Greeting(long l, String content) {
+		this.id = l;
+		this.content = content;
+		
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+}
